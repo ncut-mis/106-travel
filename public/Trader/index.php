8d1355ecaf2f0454bf2fb56118cf36451dff812d
Roy <lklkpps123456@gmail.com>
2020-04-08 03:24:21 +0800

    新增php&trader資料夾

---
+++ public/Trader/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+    <title>平台業者畫面</title>
+</head>
+<body>
+    
+<div style = "height:50px;width:100%;background-color : black;margin-bottom:20px;color:white;padding:10px;text-align:center"><h3>平台業者</h3></div>
+<div class="container">
+    <div class="row align-items-center text-center ">
+        <div class="col-12"><a  class="btn btn-primary" href="b1.php">查詢每筆交易紀錄</a></div></br></br></br>
+        <div class="col-12"><a  class="btn btn-secondary" href="b2.php">導遊審核</a></div></br></br></br>
+        <div class="col-12"><a  class="btn btn-success" href="b3.php">黑名單</a></div></br></br></br>
+        <div class="col-12"><a  class="btn btn btn-danger" href="b4.php">營收監控</a></div></br></br></br>
+        <div class="col-12"><a  class="btn btn-info" href="b5.php">內部資訊監控</a></div></br></br></br>
+        
+    </div>
+</div>
+
+
+
+
+
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+</body>
+</html>
