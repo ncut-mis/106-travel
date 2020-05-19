d8988f15006825477e8efde0f74fb2292ba6ea61
Roy <lklkpps123456@gmail.com>
2020-05-20 01:48:41 +0800

    datetime修改為date

--- database/migrations/2020_04_25_102315_create_guides_audit_table.php
+++ database/migrations/2020_04_25_102315_create_guides_audit_table.php
@@ -14,12 +14,13 @@ class CreateGuidesAuditTable extends Migration
     public function up()
     {
         Schema::create('guides_audit', function (Blueprint $table) {
-            $table->integer('guide_id');
-            $table->string('license_intro');//證照簡介
-            $table->string('license');//圖片路徑
+            $table->bigIncrements('id');
+            $table->string('license_intro')->nullable();//證照簡介
+            $table->string('license')->nullable();//圖片路徑
             $table->string('video_intro')->nullable();//影片簡介
             $table->string('video')->nullable();//影片路徑
             $table->text('motive')->nullable(); ;//假如沒有圖片,影片的話
+            $table->integer('guide_id');
         });
     }
 
