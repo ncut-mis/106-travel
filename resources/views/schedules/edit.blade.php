35a35770c608dd4398754ca93988344f60923048
Lin Ming Ying <moon9638150@gmail.com>
2020-05-26 23:33:10 +0800

    會員編輯畫面修正

--- resources/views/schedules/edit.blade.php
+++ resources/views/schedules/edit.blade.php
@@ -21,6 +21,7 @@
         <div class="form-group">
             <label for="location">旅遊區域:</label>
             <select type="text" class="selectpicker" name="select_region" id="select_region" style="width:100px; height:50px;" value="">
+                <option value="">請選擇縣市</option>
                 <option value="基隆市">基隆市</option>
                 <option value="台北市">台北市</option>
                 <option value="新北市">新北市</option>
@@ -59,32 +60,47 @@
 {{--        </div>--}}
 
 
+        <script type="text/javascript">
+            //alert("hello")
+            $(document).ready(function() {
+                var i = $('#select_region').change(function () {
+                    $text=$('#select_region').val();
+                    //var text = $(this).text()
+                    document.getElementById("update_region").value=$text
+                    //alert($text)
+
+                })});
+        </script>
+
+            <div  class="form-group" >
+                <label class="control-label col-sm-2" >目前選擇的旅遊區域為:</label>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" id="update_region" name="update_region" style="width:100px; height:50px;" readonly="readonly" value={{$b1->region}}>
+                </div>
+            </div>
+        <script type="text/javascript">
+            //取值
+            $(document).ready(function() {
+
+                    $(".click").click(function(){
+                        ($("#update_region").val());
+                        $("#select_region").val($("#update_region").val())
+                })
+            });
+
+
+        </script>
 {{--        <script type="text/javascript">--}}
-{{--            //alert("hello")--}}
-{{--            $(document).ready(function()--}}
-{{--            {--}}
-{{--                     //var i = $('dropdown-menu a').click(function () {--}}
-{{--                    // var text =$(this).text()--}}
-{{--                     //$('#update_region').text(text)--}}
-{{--                     //alert(text)--}}
+{{--            //設值--}}
+{{--            $(document).ready(function() {--}}
 
-
+{{--                $(".click").click(function(){--}}
+{{--                    ($("#update_region").val(#update_region));--}}
+{{--                })--}}
 {{--            });--}}
 
+
 {{--        </script>--}}
-
-
-
-        @if($b1->region == NUll)
-        @else
-        <div  class="form-group" >
-            <label class="control-label col-sm-2" >目前選擇的旅遊區域為:</label>
-            <div class="col-sm-10">
-
-               <input type="text" class="form-control" id="update_region" name="update_region" style="width:100px; height:50px;" readonly="readonly"  value={{$b1->region}}>
-            </div>
-        </div>
-        @endif
 {{--        google地圖--}}
 {{--        <iframe src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d1872629.7674644934!2d120.6786654!3d23.5511977!3m2!1i1024!2i768!4f13.1!5e0!3m2!1szh-TW!2stw!4v1588430013281!5m2!1szh-TW!2stw" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>--}}
 
@@ -140,7 +156,7 @@
                 <input type="hidden" class="form-control" id="update_guide_id" name="update_guide_id" style="width:1550px; height:100px;" value={{$b1->guide_id}}>
             </div>
         </div>
-        <button type="submit" class="btn btn-default">儲存</button>
+        <button type="submit" class="click">儲存</button>
     </form>
     @if($b1->guide_id != "")
         <label class="">目前已有媒合導遊</label>
