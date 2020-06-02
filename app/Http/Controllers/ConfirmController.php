db994ed0f48dcf55409b5063dc750d2f103e01fd
3A632095 <yusufiscool113@gmail.com>
2020-06-02 19:16:54 +0800

    把dd刪掉

--- app/Http/Controllers/ConfirmController.php
+++ app/Http/Controllers/ConfirmController.php
@@ -26,7 +26,6 @@ class ConfirmController extends Controller
 
     {
         $howtopay=$request->input('paymethods');
-        dd($howtopay);
         $total=$request->input('total');
         $today= date ("Y-m-d H:i:s" , mktime(date('H'), date('i'), date('s'), date('m'), date('d'), date('Y'))) ;
         $needconfirmtravel=Travel::find($request->input('travel_id'));
